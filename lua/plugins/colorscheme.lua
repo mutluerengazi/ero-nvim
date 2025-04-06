a3c277ddad4322acfc5b1070cc05d1587ac0c66c
erengazi <mutluerengazi@gmail.com>
2025-04-07 00:37:41 +0300

    rosepine hopefully last

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	"catppuccin/nvim",
+	"rose-pine/neovim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("catppuccin-mocha")
+		vim.cmd.colorscheme("rose-pine-moon")
 	end,
 }
