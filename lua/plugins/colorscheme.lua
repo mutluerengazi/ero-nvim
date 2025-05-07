5359b04e4fb0c161bae2e770d3fe4e9c605db847
erengazi <mutluerengazi@gmail.com>
2025-05-07 03:01:29 +0300

    feat update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
 return {
-  "folke/tokyonight.nvim",
+  'sainnhe/gruvbox-material',
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("tokyonight-moon")
-  end,
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    vim.g.gruvbox_material_enable_italic = true
+    vim.cmd.colorscheme('gruvbox-material')
+  end
 }
