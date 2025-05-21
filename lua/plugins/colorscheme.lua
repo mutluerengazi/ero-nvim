c41880050c6e91fc5f86a44c3bc760002492f2c4
erengazi <mutluerengazi@gmail.com>
2025-05-21 13:47:07 +0300

    feat: update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "gbprod/nord.nvim",
+  "catppuccin/nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme('nord')
+    vim.cmd.colorscheme('catppuccin-mocha')
   end
 }
